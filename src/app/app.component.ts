a23475f631e620ef67b3f71552fb093a822ad87e
gmorklla <gmorklla@gmail.com>
2017-02-10 13:00:05 -0600

    Select for change the http request with event emmited for complete status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpGetService } from './http-get.service';
 import { EmitterService } from './emitter.service';
 
@@ -7,36 +7,65 @@ import { EmitterService } from './emitter.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit {
 
-    title = 'Http request';
     datos;
-    familia: string = 'MEDICAMENTOS';
+    familia: string;
+
+    familias = [
+        {value: 'MEDICAMENTOS', viewValue: 'Medicamentos'},
+        {value: 'VITAMINAS Y SUPLEMENTOS', viewValue: 'Vitaminas'},
+        {value: 'HIGIENE Y PERFUMERIA', viewValue: 'Higiene'},
+        {value: 'MATERIAL DE CURACION', viewValue: 'Material de curación'}
+    ];
 
     constructor(private httpGet: HttpGetService) {
     }
 
     ngOnInit() {
-    //this.datos = this.httpGet.getMeds();
+        //this.datos = this.httpGet.getMeds();
+
+        this.familia = 'MEDICAMENTOS';
 
         this.httpGet.getMeds(this.familia).subscribe(
-            result => { 
-                this.getData( result ); 
+            result => {
+                this.getData( result );
             },
             error  => console.error( error ) );
-        
+
         EmitterService.get('MEDICAMENTOS').subscribe((res) => {
             console.log(res);
-        });        
+        });
 
-    }
+        EmitterService.get('VITAMINAS Y SUPLEMENTOS').subscribe((res) => {
+            console.log(res);
+        });
 
-    ngOnChanges() {
-                
+        EmitterService.get('HIGIENE Y PERFUMERIA').subscribe((res) => {
+            console.log(res);
+        });
+
+        EmitterService.get('MATERIAL DE CURACION').subscribe((res) => {
+            console.log(res);
+        });
+
     }
 
     getData(data) {
         this.datos = data;
+    }
+
+    setFamilia(familia: string) {
+        this.familia = familia;
+        this.getProducts();
+    }
+
+    private getProducts() {
+        this.httpGet.getMeds(this.familia).subscribe(
+            result => {
+                this.getData( result );
+            },
+            error  => console.error( error ) );
     }
 
 }
